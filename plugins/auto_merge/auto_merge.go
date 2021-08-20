f1db39a9c010d6624e635a6bc009d016ed32240d
Anton Bracke <anton@ju60.de>
2021-08-20 16:50:45 +0200

    fix: rerun checks if a mr-pipeline finishes

--- plugins/auto_merge/auto_merge.go
+++ plugins/auto_merge/auto_merge.go
@@ -29,15 +29,6 @@ func (plugin autoMergePlugin) Name() string {
 }
 
 func (plugin autoMergePlugin) Execute(project *gitlab.Project, config config.ProjectConfig) {
-	opt := &gitlab.ListProjectMergeRequestsOptions{
-		State:        gitlab.String("opened"),
-		UpdatedAfter: plugin.lastestChecks[project.ID],
-		// TargetBranch: &project.DefaultBranch,
-		ListOptions: gitlab.ListOptions{
-			PerPage: 10,
-			Page:    1,
-		},
-	}
 
 	err := json.Unmarshal(config.Plugins[plugin.Name()], &plugin.loadedConfig)
 	if err != nil {
@@ -46,34 +37,16 @@ func (plugin autoMergePlugin) Execute(project *gitlab.Project, config config.Pro
 	}
 	log.Debugf("Loaded config: %+v", plugin.loadedConfig)
 
-	totalMergeRequests := 0
 	now := time.Now()
 
-	for {
-		// Get the first page with merge-requests
-		mergeRequests, resp, err := plugin.Client.MergeRequests.ListProjectMergeRequests(project.ID, opt)
-		if err != nil {
-			log.Debug("Can't load merge-requests", err)
-		}
+	mergeRequests := plugin.getUpdatedMergeRequests(project)
+	mergeRequests = append(mergeRequests, plugin.getUpdatedPipelineMergeRequests(project)...)
 
-		log.Debugf("checking %d merge-requests ...\n", len(mergeRequests))
-
-		totalMergeRequests = totalMergeRequests + len(mergeRequests)
-
-		for _, mergeRequest := range mergeRequests {
-			plugin.autoMerge(project, mergeRequest)
-		}
-
-		// Exit the loop when we've seen all pages
-		if resp.CurrentPage >= resp.TotalPages {
-			break
-		}
-
-		// Update the page number to get the next page
-		opt.Page = resp.NextPage
+	for _, mergeRequest := range mergeRequests {
+		plugin.autoMerge(project, mergeRequest)
 	}
 
-	log.Debugf("checked total of %d merge-requests\n", totalMergeRequests)
+	log.Debugf("checked total of %d merge-requests\n", len(mergeRequests))
 
 	plugin.lastestChecks[project.ID] = &now
 }
@@ -104,4 +77,94 @@ func (plugin autoMergePlugin) autoMerge(project *gitlab.Project, mergeRequest *g
 	}
 
 	plugin.updateStatusComment(project, mergedMergeRequest, status)
+}
+
+func (plugin autoMergePlugin) getUpdatedPipelineMergeRequests(project *gitlab.Project) []*gitlab.MergeRequest {
+	var mergeRequests []*gitlab.MergeRequest
+
+	lastCheck := *plugin.lastestChecks[project.ID]
+
+	opt := &gitlab.ListProjectPipelinesOptions{
+		UpdatedAfter: &lastCheck,
+		ListOptions: gitlab.ListOptions{
+			PerPage: 10,
+			Page:    1,
+		},
+	}
+
+	for {
+		pipelines, resp, err := plugin.Client.Pipelines.ListProjectPipelines(project.ID, opt)
+		if err != nil {
+			log.Debug("Can't load pipelines", err)
+		}
+
+		for _, pipeline := range pipelines {
+			optMR := &gitlab.ListProjectMergeRequestsOptions{
+				State:        gitlab.String("opened"),
+				SourceBranch: &pipeline.Ref,
+				ListOptions: gitlab.ListOptions{
+					PerPage: 10,
+					Page:    1,
+				},
+			}
+
+			_mergeRequests, _, err := plugin.Client.MergeRequests.ListProjectMergeRequests(project.ID, optMR)
+			if err != nil {
+				log.Debug("Can't load merge-requests", err)
+			}
+
+			if len(_mergeRequests) < 1 {
+				log.Debug("No related merge-request not found")
+			} else {
+				// if one or more check all merge-requests
+				mergeRequests = append(mergeRequests, _mergeRequests...)
+			}
+		}
+
+		// Exit the loop when we've seen all pages
+		if resp.CurrentPage >= resp.TotalPages {
+			break
+		}
+
+		// Update the page number to get the next page
+		opt.Page = resp.NextPage
+	}
+
+	return mergeRequests
+}
+
+func (plugin autoMergePlugin) getUpdatedMergeRequests(project *gitlab.Project) []*gitlab.MergeRequest {
+	var mergeRequests []*gitlab.MergeRequest
+
+	lastCheck := *plugin.lastestChecks[project.ID]
+
+	opt := &gitlab.ListProjectMergeRequestsOptions{
+		State: gitlab.String("opened"),
+		// TargetBranch: &project.DefaultBranch,
+		UpdatedAfter: &lastCheck,
+		ListOptions: gitlab.ListOptions{
+			PerPage: 10,
+			Page:    1,
+		},
+	}
+
+	for {
+		// Get the first page with merge-requests
+		_mergeRequests, resp, err := plugin.Client.MergeRequests.ListProjectMergeRequests(project.ID, opt)
+		if err != nil {
+			log.Debug("Can't load merge-requests", err)
+		}
+
+		mergeRequests = append(mergeRequests, _mergeRequests...)
+
+		// Exit the loop when we've seen all pages
+		if resp.CurrentPage >= resp.TotalPages {
+			break
+		}
+
+		// Update the page number to get the next page
+		opt.Page = resp.NextPage
+	}
+
+	return mergeRequests
 }
