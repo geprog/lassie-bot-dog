da52d274bc42d5fa48b7628d2a76e3d3dd20e2cd
Anton Bracke <anton@ju60.de>
2021-08-23 16:35:07 +0200

    fix: load merge-request from ref if pipeline updated

---
+++ plugins/auto_merge/auto_merge_test.go
@@ -0,0 +1,33 @@
+package auto_merge_test
+
+import (
+	"testing"
+
+	"github.com/GEPROG/lassie-bot-dog/plugins/auto_merge"
+)
+
+func TestIsRefMergeRequest(t *testing.T) {
+	ref := "refs/merge-requests/50/head"
+	if !auto_merge.IsRefMergeRequest(ref) {
+		t.Error()
+	}
+
+	ref = "refs/main"
+	if auto_merge.IsRefMergeRequest(ref) {
+		t.Error()
+	}
+}
+
+func TestGetMergeRequestIdFromRef(t *testing.T) {
+	ref := "refs/merge-requests/50/head"
+	mergeRequestIID, err := auto_merge.GetMergeRequestIdFromRef(ref)
+	if mergeRequestIID != 50 || err != nil {
+		t.Error()
+	}
+
+	ref = "refs/main"
+	mergeRequestIID, err = auto_merge.GetMergeRequestIdFromRef(ref)
+	if mergeRequestIID != -1 || err == nil {
+		t.Error()
+	}
+}
