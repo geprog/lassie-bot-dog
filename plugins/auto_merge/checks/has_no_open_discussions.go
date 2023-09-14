af1949de0259b9dc68c17c243c211d415fbefde6
Lukas <lukas@slucky.de>
2023-09-14 11:47:40 +0200

    fix: use notes endpoint to check for unresolved discussions (#68)

--- plugins/auto_merge/checks/has_no_open_discussions.go
+++ plugins/auto_merge/checks/has_no_open_discussions.go
@@ -2,14 +2,28 @@ package checks
 
 import (
 	"github.com/GEPROG/lassie-bot-dog/plugins/auto_merge/config"
+	"github.com/GEPROG/lassie-bot-dog/utils"
 	"github.com/xanzy/go-gitlab"
 )
 
 type HasNoOpenDiscussionsCheck struct {
+	Client *gitlab.Client
 }
 
-func (check HasNoOpenDiscussionsCheck) Check(_ *config.AutoMergeConfig, _ *gitlab.Project, mergeRequest *gitlab.MergeRequest) bool {
-	return mergeRequest.BlockingDiscussionsResolved
+func (check HasNoOpenDiscussionsCheck) Check(_ *config.AutoMergeConfig, project *gitlab.Project, mergeRequest *gitlab.MergeRequest) bool {
+	log := utils.Logger(project, mergeRequest)
+	notes, _, err := check.Client.Notes.ListMergeRequestNotes(mergeRequest.ProjectID, mergeRequest.IID, nil)
+	if err != nil {
+		log.Error("Can't load merge-request notes", err)
+		return false
+	}
+	for _, note := range notes {
+		if note.Resolvable && !note.Resolved {
+			log.Debug("Found unresolved discussion", note.ID)
+			return false
+		}
+	}
+	return true
 }
 
 func (check HasNoOpenDiscussionsCheck) Name() string {
