d876e55ac2a7278dd0b85dbca5b387dc4b7dde1c
Lukas Hass <lukas@slucky.de>
2024-02-08 19:55:26 +0100

    feat: add milestone set check

---
+++ plugins/auto_merge/checks/has_milestone.go
@@ -0,0 +1,25 @@
+package checks
+
+import (
+	"github.com/GEPROG/lassie-bot-dog/plugins/auto_merge/config"
+	"github.com/xanzy/go-gitlab"
+)
+
+type HasMilestone struct {
+}
+
+func (check HasMilestone) Check(_ *config.AutoMergeConfig, _ *gitlab.Project, mergeRequest *gitlab.MergeRequest) bool {
+	return mergeRequest.Milestone != nil
+}
+
+func (check HasMilestone) Name() string {
+	return "has-milestone"
+}
+
+func (check HasMilestone) PassedText(_ int) string {
+	return "Your Merge-Request has a milestone assigned"
+}
+
+func (check HasMilestone) FailedText(_ int) string {
+	return "Your Merge-Request has no milestone assigned"
+}
